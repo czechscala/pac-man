b51356edb3fba04eb9d6d0b19c4bd45423e45d16
Janeček Jakub <janecek.jakub@gmail.com>
2014-07-29 20:50:15 +0200

    Add Engine

---
+++ src/main/scala/com/czechscala/pacman/PacManGame.scala
@@ -0,0 +1,62 @@
+package com.czechscala.pacman
+
+import akka.actor.{Actor, ActorSystem, Props}
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+
+object PacManGame extends App {
+
+  case object Tick
+
+  case object NewGame
+
+  case class Key(code: Int)
+
+  class Engine extends Actor {
+    private val renderer = new Swing(6, 5, self)
+    private var state = Board(initialGrid, Map(), Map(PacMan -> (Position(1, 1), Some(Right))))
+    private var delay = 1000
+
+    override def preStart = {
+      render
+      tick
+    }
+
+    private def tick = context.system.scheduler.scheduleOnce(delay millis, self, Tick)
+
+    def receive = {
+      // case NewGame => state = State(Board(20, 30, Set()), None)
+
+      case Tick =>
+        state = state.moveAllCharacters
+        render
+        tick
+
+      case dir: Direction =>
+        state = state.setPacManDirection(dir)
+        render
+    }
+
+    private def render = renderer render state
+
+    def getDelay = delay
+  }
+
+  val system = ActorSystem("pacman")
+  val engine = system.actorOf(Props[Engine])
+
+  val initialGrid = parse(
+    "######\n" +
+    "#    #\n" +
+    "#    #\n" +
+    "#    #\n" +
+    "######\n"
+  )
+
+  def parse(gridStr: String): Array[Array[Cell]] = gridStr split "\n" map (_ map {
+    case '#' => Wall
+    case _ => Empty
+  } toArray)
+
+}
