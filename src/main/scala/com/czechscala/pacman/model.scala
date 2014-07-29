ae3c19a5097691a7ca3c715265e9b50bab259fff
Karel Smutný <karel.smutny@outlook.com>
2014-07-29 19:14:20 +0200

    added model classes (again)

--- src/main/scala/com/czechscala/pacman/model.scala
+++ src/main/scala/com/czechscala/pacman/model.scala
@@ -1,5 +1,29 @@
 package com.czechscala.pacman
 
-package object model {
-
+case class Board(
+                  grid: Array[Array[Cell]],
+                  objects: Map[Position, Object],
+                  characters: Map[Character, (Position, Direction)]
+                  ) {
+  def width = grid(0).length
+  def height = grid.length
 }
+
+case class Position(x: Int, y: Int)
+
+trait Object
+class Gem extends Object
+
+trait Cell
+case object Wall extends Cell
+case object Empty extends Cell
+
+trait Character
+case object PacMan extends Character
+class Ghost extends Character
+
+sealed trait Direction
+case object Up extends Direction
+case object Down extends Direction
+case object Left extends Direction
+case object Right extends Direction
